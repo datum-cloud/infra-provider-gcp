749e563da937c8cc1e4de063b43091e937774273
Joshua Reese <joshlreese@gmail.com>
2025-05-22 19:28:57 -0500

    feat: Direct lifecycle control of GCP instances.
    These changes remove the use of managed instance groups, and instead directly
    provisions and manages the lifecycle of GCP instances. This move depends on
    changes in workload-operator to manage the lifecycle of `Instance` resources,
    and changes in network-services-operator to manage Subnet and Network resources.
    
    Instead of watching for `WorkloadDeployment` resources and creating managed
    instance groups, the provider will now watch for `Instance` resources and create
    and manage the underlying GCP instances. If the instance has any scheduling
    gates, it will not be processed. Most of the logic in the new instance
    controller is a port of logic in the previous workload deployment controller.
    While instances are reconciled directly, there are some resources such as
    service accounts and secrets that are managed at the workload level workload.
    In these cases, there is an additional controller to handle garbage collection
    when appropriate.
    
    Creation of SubnetClaims has been removed, and responsibility of creating those
    moved to workload-operator. Subnet provisioning has been moved to a dedicated
    controller, reacting to `Subnets` that are maintained by
    network-services-operator.
    
    Provisioning of GCP networks has been moved to a new controller that watches for
    `NetworkContexts`. One or more NetworkContexts may attach to the same Datum
    Network, which will be provisioned as a single VPC network at GCP.
    
    Other key changes:
    
    - Moved from leveraging k8s-config-connector to Crossplane for managing
      resources. The key benefit here is that a single deployment of Crossplane can
      support authenticating individual resources with different credentials. This
      significantly reduces per-project infrastructure overhead. The security
      posture remains the same, with the use of Workload Identity and no service
      account keys being provisioned for managing GCP resources.
    - Adoption of the multicluster-runtime library to support deploying an operator
      which can handle multiple clusters.
    - Added structured configuration with sane defaults, migrated some flags to
      config settings.
    - Added upstream, downstream, and deployment RBAC definitions.
    - Each new controller is responsible for maintaining a `Programmed` condition on
      relevant resources to communicate to other parts of the system whether or not
      the expected changes have been applied.
    - Provisioning of additional disk backed volumes is currently not supported.
    
    Callout:
    
    Test coverage for these changes is currently low. During development, a design
    pattern that we'd like to start leveraging was discovered, which will not only
    make testing simpler, but also bring greater visibility into the expected
    behavior of each controller. A followup PR will refactor the controllers to
    use the new pattern, and bring tests at that time.

--- go.mod
+++ go.mod
@@ -1,40 +1,46 @@
 module go.datum.net/infra-provider-gcp
 
-go 1.23.0
+go 1.23.8
+
+toolchain go1.24.1
 
 require (
-	cloud.google.com/go/compute v1.29.0
-	github.com/GoogleCloudPlatform/k8s-config-connector v1.125.0
+	github.com/crossplane/crossplane-runtime v1.17.0
 	github.com/go-logr/logr v1.4.2
-	github.com/googleapis/gax-go/v2 v2.14.0
-	github.com/onsi/ginkgo/v2 v2.19.0
-	github.com/onsi/gomega v1.33.1
-	github.com/stretchr/testify v1.9.0
-	go.datum.net/network-services-operator v0.0.0-20241205161241-d19fee49f0b7
-	go.datum.net/workload-operator v0.0.0-20241205164213-161fb5150fb8
-	google.golang.org/protobuf v1.35.1
+	github.com/stretchr/testify v1.10.0
+	github.com/upbound/provider-gcp v1.13.0
+	go.datum.net/network-services-operator v0.2.2-0.20250524001505-171de7683bae
+	go.datum.net/workload-operator v0.3.3-0.20250529002934-93284e87ec50
+	golang.org/x/sync v0.13.0
 	gopkg.in/yaml.v3 v3.0.1
-	k8s.io/api v0.31.1
-	k8s.io/apimachinery v0.31.1
-	k8s.io/apiserver v0.31.1
-	k8s.io/client-go v0.31.1
-	sigs.k8s.io/controller-runtime v0.19.1
-	sigs.k8s.io/gateway-api v1.2.0
+	k8s.io/api v0.32.1
+	k8s.io/apimachinery v0.32.1
+	k8s.io/client-go v0.32.1
+	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738
+	sigs.k8s.io/controller-runtime v0.20.4
+	sigs.k8s.io/multicluster-runtime v0.20.4-alpha.6
 )
 
 require (
-	cloud.google.com/go/auth v0.10.1 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.5 // indirect
-	cloud.google.com/go/compute/metadata v0.5.2 // indirect
+	cel.dev/expr v0.18.0 // indirect
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/crossplane/upjet v1.4.1-0.20250108142216-db86f70a1651 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/emicklei/go-restful/v3 v3.12.0 // indirect
-	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.12.1 // indirect
+	github.com/evanphx/json-patch v5.9.0+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fatih/color v1.17.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
@@ -43,24 +49,40 @@ require (
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/gobuffalo/flect v1.0.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/cel-go v0.20.1 // indirect
+	github.com/google/btree v1.1.3 // indirect
+	github.com/google/cel-go v0.22.0 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/pprof v0.0.0-20240528025155-186aa0362fba // indirect
-	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
-	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
+	github.com/hashicorp/go-hclog v1.6.3 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.20.1 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/hashicorp/terraform-json v0.22.1 // indirect
+	github.com/hashicorp/terraform-plugin-framework v1.7.0 // indirect
+	github.com/hashicorp/terraform-plugin-go v0.23.0 // indirect
+	github.com/hashicorp/terraform-plugin-log v0.9.0 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.33.0 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -70,11 +92,17 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	github.com/tmccombs/hcl2json v0.3.4 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	go.opencensus.io v0.24.0 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	github.com/zclconf/go-cty v1.14.4 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
 	go.opentelemetry.io/otel v1.29.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
@@ -84,33 +112,34 @@ require (
 	go.opentelemetry.io/otel/trace v1.29.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/crypto v0.35.0 // indirect
-	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
-	golang.org/x/net v0.36.0 // indirect
-	golang.org/x/oauth2 v0.23.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/term v0.29.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	golang.org/x/time v0.7.0 // indirect
-	golang.org/x/tools v0.24.0 // indirect
+	golang.org/x/tools v0.31.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
-	google.golang.org/api v0.205.0 // indirect
-	google.golang.org/genproto v0.0.0-20241021214115-324edc3d5d38 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241104194629-dd2ea8efbc28 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241104194629-dd2ea8efbc28 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apiextensions-apiserver v0.31.1 // indirect
-	k8s.io/component-base v0.31.1 // indirect
+	k8s.io/apiextensions-apiserver v0.32.1 // indirect
+	k8s.io/apiserver v0.32.1 // indirect
+	k8s.io/component-base v0.32.1 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20240423202451-8948a665c108 // indirect
-	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
-	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.30.3 // indirect
-	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20241105132330-32ad38e42d3f // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.31.0 // indirect
+	sigs.k8s.io/controller-tools v0.16.3 // indirect
+	sigs.k8s.io/gateway-api v1.2.1 // indirect
+	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.2 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- internal/controller/networkcontext_controller.go
+++ internal/controller/networkcontext_controller.go
@@ -4,59 +4,64 @@ package controller
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
-	kcccomputev1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/generated/apis/compute/v1beta1"
-	kcccomputev1alpha1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/generated/apis/k8s/v1alpha1"
-	"google.golang.org/protobuf/proto"
+	crossplanecommonv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	gcpcomputev1beta1 "github.com/upbound/provider-gcp/apis/compute/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
-	"sigs.k8s.io/controller-runtime/pkg/finalizer"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	mcbuilder "sigs.k8s.io/multicluster-runtime/pkg/builder"
+	mccontext "sigs.k8s.io/multicluster-runtime/pkg/context"
+	mcmanager "sigs.k8s.io/multicluster-runtime/pkg/manager"
+	mcreconcile "sigs.k8s.io/multicluster-runtime/pkg/reconcile"
 
-	"go.datum.net/infra-provider-gcp/internal/controller/k8sconfigconnector"
-	"go.datum.net/infra-provider-gcp/internal/crossclusterutil"
+	"go.datum.net/infra-provider-gcp/internal/config"
+	"go.datum.net/infra-provider-gcp/internal/downstreamclient"
 	"go.datum.net/infra-provider-gcp/internal/locationutil"
+	datumsource "go.datum.net/infra-provider-gcp/internal/source"
 	networkingv1alpha "go.datum.net/network-services-operator/api/v1alpha"
 )
 
 // NetworkContextReconciler reconciles a NetworkContext and ensures that a GCP
 // ComputeNetwork is created to represent the context within GCP.
 type NetworkContextReconciler struct {
-	client.Client
-	InfraClient               client.Client
-	Scheme                    *runtime.Scheme
-	LocationClassName         string
-	InfraClusterNamespaceName string
-
-	finalizers finalizer.Finalizers
+	mgr               mcmanager.Manager
+	Config            config.GCPProvider
+	DownstreamCluster cluster.Cluster
+	LocationClassName string
 }
 
-// +kubebuilder:rbac:groups=compute.datumapis.com,resources=networkcontexts,verbs=get;list;watch
-// +kubebuilder:rbac:groups=compute.datumapis.com,resources=networkcontexts/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=compute.datumapis.com,resources=networkcontexts/finalizers,verbs=update
-
-// +kubebuilder:rbac:groups=compute.cnrm.cloud.google.com,resources=computenetworks,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=compute.cnrm.cloud.google.com,resources=computenetworks/status,verbs=get
-
-func (r *NetworkContextReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, err error) {
+func (r *NetworkContextReconciler) Reconcile(ctx context.Context, req mcreconcile.Request) (_ ctrl.Result, err error) {
 	logger := log.FromContext(ctx)
 
+	cl, err := r.mgr.GetCluster(ctx, req.ClusterName)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	ctx = mccontext.WithCluster(ctx, req.ClusterName)
+
 	var networkContext networkingv1alpha.NetworkContext
-	if err := r.Client.Get(ctx, req.NamespacedName, &networkContext); err != nil {
+	if err := cl.GetClient().Get(ctx, req.NamespacedName, &networkContext); err != nil {
 		if apierrors.IsNotFound(err) {
 			return ctrl.Result{}, nil
 		}
 		return ctrl.Result{}, err
 	}
 
-	location, shouldProcess, err := locationutil.GetLocation(ctx, r.Client, networkContext.Spec.Location, r.LocationClassName)
+	location, shouldProcess, err := locationutil.GetLocation(ctx, cl.GetClient(), networkContext.Spec.Location, r.LocationClassName)
 	if err != nil {
 		return ctrl.Result{}, err
 	} else if !shouldProcess {
@@ -66,38 +71,26 @@ func (r *NetworkContextReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	logger.Info("reconciling network context")
 	defer logger.Info("reconcile complete")
 
-	finalizationResult, err := r.finalizers.Finalize(ctx, &networkContext)
-	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to finalize: %w", err)
-	}
-	if finalizationResult.Updated {
-		if err = r.Client.Update(ctx, &networkContext); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update based on finalization result: %w", err)
-		}
-		return ctrl.Result{}, nil
-	}
-
 	if !networkContext.DeletionTimestamp.IsZero() {
 		return ctrl.Result{}, nil
 	}
 
-	readyCondition := metav1.Condition{
-		Type:               networkingv1alpha.NetworkBindingReady,
+	if apimeta.IsStatusConditionTrue(networkContext.Status.Conditions, networkingv1alpha.NetworkContextProgrammed) {
+		logger.Info("network context is already programmed")
+		return ctrl.Result{}, nil
+	}
+
+	programmedCondition := metav1.Condition{
+		Type:               networkingv1alpha.NetworkContextProgrammed,
 		Status:             metav1.ConditionFalse,
-		Reason:             "Unknown",
+		Reason:             networkingv1alpha.NetworkContextProgrammedReasonNotProgrammed,
 		ObservedGeneration: networkContext.Generation,
-		Message:            "Unknown state",
+		Message:            "Network has not been programmed",
 	}
 
 	defer func() {
-		if err != nil {
-			// Don't update the status if errors are encountered
-			return
-		}
-		statusChanged := apimeta.SetStatusCondition(&networkContext.Status.Conditions, readyCondition)
-
-		if statusChanged {
-			err = r.Client.Status().Update(ctx, &networkContext)
+		if apimeta.SetStatusCondition(&networkContext.Status.Conditions, programmedCondition) {
+			err = errors.Join(err, cl.GetClient().Status().Update(ctx, &networkContext))
 		}
 	}()
 
@@ -106,87 +99,141 @@ func (r *NetworkContextReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		Namespace: networkContext.Namespace,
 		Name:      networkContext.Spec.Network.Name,
 	}
-	if err := r.Client.Get(ctx, networkObjectKey, &network); err != nil {
+	if err := cl.GetClient().Get(ctx, networkObjectKey, &network); err != nil {
 		return ctrl.Result{}, fmt.Errorf("failed fetching network: %w", err)
 	}
 
-	kccNetworkName := fmt.Sprintf("network-%s", networkContext.UID)
-
-	var kccNetwork kcccomputev1beta1.ComputeNetwork
-	kccNetworkObjectKey := client.ObjectKey{
-		Namespace: r.InfraClusterNamespaceName,
-		Name:      kccNetworkName,
+	// Note!!!
+	//
+	// Crossplane v1 managed resources are cluster scoped! We don't want
+	// to deal with the creation of Composite Resource Definitions, Composite
+	// Resources, or Claims. As a result, the resource name needs to be globally
+	// unique.
+	//
+	// In Crossplane v2, which is currently in preview, managed resources will
+	// also be available as namespaced resources. Unfortunately the preview does
+	// not include modifications to the GCP provider.
+	//
+	// If we want the name to be used for the resource at the provider to differ
+	// from this resource name, use the following annotation:
+	// crossplanemeta.AnnotationKeyExternalName (crossplane.io/external-name)
+	downstreamNetwork := &gcpcomputev1beta1.Network{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("network-%s", network.UID),
+		},
 	}
-	if err := r.InfraClient.Get(ctx, kccNetworkObjectKey, &kccNetwork); client.IgnoreNotFound(err) != nil {
-		return ctrl.Result{}, fmt.Errorf("failed fetching gcp network: %w", err)
-	}
 
-	if kccNetwork.CreationTimestamp.IsZero() {
-		logger.Info("creating GCP network")
+	result, err := controllerutil.CreateOrPatch(ctx, r.DownstreamCluster.GetClient(), downstreamNetwork, func() error {
+		if downstreamNetwork.Annotations == nil {
+			downstreamNetwork.Annotations = make(map[string]string)
+		}
 
-		kccNetwork = kcccomputev1beta1.ComputeNetwork{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: kccNetworkObjectKey.Namespace,
-				Name:      kccNetworkObjectKey.Name,
-				Annotations: map[string]string{
-					GCPProjectAnnotation: location.Spec.Provider.GCP.ProjectID,
+		downstreamNetwork.Annotations[downstreamclient.UpstreamOwnerClusterName] = req.ClusterName
+
+		downstreamNetwork.Spec = gcpcomputev1beta1.NetworkSpec{
+			ResourceSpec: crossplanecommonv1.ResourceSpec{
+				ProviderConfigReference: &crossplanecommonv1.Reference{
+					Name: r.Config.GetProviderConfigName(req.ClusterName),
 				},
 			},
-			Spec: kcccomputev1beta1.ComputeNetworkSpec{
-				Mtu: proto.Int64(int64(network.Spec.MTU)),
+			ForProvider: gcpcomputev1beta1.NetworkParameters{
+				Mtu:                                   ptr.To(float64(network.Spec.MTU)),
+				AutoCreateSubnetworks:                 ptr.To(false),
+				NetworkFirewallPolicyEnforcementOrder: ptr.To("AFTER_CLASSIC_FIREWALL"),
+				RoutingMode:                           ptr.To("REGIONAL"),
+				Project:                               ptr.To(location.Spec.Provider.GCP.ProjectID),
 			},
 		}
 
-		kccNetwork.Spec.AutoCreateSubnetworks = proto.Bool(false)
+		return nil
+	})
 
-		if err := crossclusterutil.SetControllerReference(ctx, r.InfraClient, &networkContext, &kccNetwork, r.Scheme); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to set controller on network context: %w", err)
+	if err != nil {
+		if apierrors.IsConflict(err) {
+			return ctrl.Result{Requeue: true}, nil
 		}
-
-		if err := r.InfraClient.Create(ctx, &kccNetwork); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed creating gcp network: %w", err)
-		}
+		return ctrl.Result{}, err
 	}
 
-	if !k8sconfigconnector.IsStatusConditionTrue(kccNetwork.Status.Conditions, kcccomputev1alpha1.ReadyConditionType) {
+	logger.Info("downstream network processed", "operation_result", result)
+
+	// See if there's been a failure to create the network.
+	//
+	// NOTE(jreese): Odd observation - the ServiceAccount failure info is in a different
+	// condition. Need to look into that.
+	if condition := downstreamNetwork.Status.GetCondition(crossplanecommonv1.TypeSynced); condition.Reason == "ReconcileError" {
+		logger.Info("network failed to create")
+		programmedCondition.Reason = "NetworkFailedToCreate"
+		programmedCondition.Message = fmt.Sprintf("Network failed to create: %s", condition.Message)
+		return ctrl.Result{}, fmt.Errorf(programmedCondition.Message)
+	}
+
+	if downstreamNetwork.Status.GetCondition(crossplanecommonv1.TypeReady).Status != corev1.ConditionTrue {
 		logger.Info("GCP network not ready yet")
-		readyCondition.Reason = "ProviderNetworkNotReady"
-		readyCondition.Message = "Network is not ready."
+
+		programmedCondition.Status = metav1.ConditionFalse
+		programmedCondition.Reason = networkingv1alpha.NetworkContextProgrammedReasonProgrammingInProgress
+		programmedCondition.Message = "Network is being programmed."
+
 		return ctrl.Result{}, nil
 	}
 
-	readyCondition.Status = metav1.ConditionTrue
-	readyCondition.Reason = "NetworkReady"
-	readyCondition.Message = "Network is ready."
+	programmedCondition.Status = metav1.ConditionTrue
+	programmedCondition.Reason = networkingv1alpha.NetworkContextProgrammed
+	programmedCondition.Message = "Network is ready."
 
 	return ctrl.Result{}, nil
 }
 
-func (r *NetworkContextReconciler) Finalize(
-	ctx context.Context,
-	obj client.Object,
-) (finalizer.Result, error) {
-
-	if err := crossclusterutil.DeleteAnchorForObject(ctx, r.Client, r.InfraClient, obj, r.InfraClusterNamespaceName); err != nil {
-		return finalizer.Result{}, fmt.Errorf("failed deleting network context anchor: %w", err)
-	}
-
-	return finalizer.Result{}, nil
-}
-
 // SetupWithManager sets up the controller with the Manager.
-func (r *NetworkContextReconciler) SetupWithManager(mgr ctrl.Manager, infraCluster cluster.Cluster) error {
-	r.finalizers = finalizer.NewFinalizers()
-	if err := r.finalizers.Register(gcpInfraFinalizer, r); err != nil {
-		return fmt.Errorf("failed to register finalizer: %w", err)
-	}
+func (r *NetworkContextReconciler) SetupWithManager(mgr mcmanager.Manager) error {
+	r.mgr = mgr
 
-	return ctrl.NewControllerManagedBy(mgr).
+	return mcbuilder.ControllerManagedBy(mgr).
 		For(&networkingv1alpha.NetworkContext{}).
-		WatchesRawSource(source.TypedKind(
-			infraCluster.GetCache(),
-			&kcccomputev1beta1.ComputeNetwork{},
-			crossclusterutil.TypedEnqueueRequestForUpstreamOwner[*kcccomputev1beta1.ComputeNetwork](mgr.GetScheme(), &networkingv1alpha.NetworkContext{}),
-		)).
+		WatchesRawSource(datumsource.MustNewClusterSource(r.DownstreamCluster, &gcpcomputev1beta1.Network{}, func(clusterName string, cl cluster.Cluster) handler.TypedEventHandler[*gcpcomputev1beta1.Network, mcreconcile.Request] {
+			return handler.TypedEnqueueRequestsFromMapFunc(func(ctx context.Context, network *gcpcomputev1beta1.Network) []mcreconcile.Request {
+				logger := log.FromContext(ctx)
+
+				upstreamClusterName := network.Annotations[downstreamclient.UpstreamOwnerClusterName]
+
+				if upstreamClusterName == "" {
+					logger.Info("GCP network is missing upstream ownership metadata")
+					return nil
+				}
+
+				cluster, err := mgr.GetCluster(ctx, upstreamClusterName)
+				if err != nil {
+					logger.Error(err, "failed to get upstream cluster")
+					return nil
+				}
+				clusterClient := cluster.GetClient()
+
+				listOpts := client.MatchingFields{
+					networkContextControllerNetworkUIDIndex: network.GetName(),
+				}
+
+				var networkContexts networkingv1alpha.NetworkContextList
+				if err := clusterClient.List(ctx, &networkContexts, listOpts); err != nil {
+					logger.Error(err, "failed to list network contexts")
+					return nil
+				}
+
+				var requests []mcreconcile.Request
+				for _, networkContext := range networkContexts.Items {
+					requests = append(requests, mcreconcile.Request{
+						Request: reconcile.Request{
+							NamespacedName: types.NamespacedName{
+								Namespace: networkContext.Namespace,
+								Name:      networkContext.Name,
+							},
+						},
+						ClusterName: upstreamClusterName,
+					})
+				}
+
+				return requests
+			})
+		})).
 		Complete(r)
 }
